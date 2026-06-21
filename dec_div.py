1cedb885a365163a825fe8822d6e141c4026c15c
Venkys-410 <n.venkateshreddy410@gmail.com>
2026-06-21 16:22:01 +0530

    dec

---
+++ dec_div.py
@@ -0,0 +1,16 @@
+
+
+def wrap(func):
+    def inner(a,b):
+        if b == 0:
+            a,b = b,a
+        return func(a,b)
+    return inner
+
+@wrap
+def div(a,b):
+    return a/b
+
+x = div(18,6)
+
+print(x)
