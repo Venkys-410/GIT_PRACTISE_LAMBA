7d3db5d2cc99a7d930881fa849a4ccafa92ff1dd
Venkys-410 <n.venkateshreddy410@gmail.com>
2026-06-21 16:13:56 +0530

    dec_div created

---
+++ dec_div.py
@@ -0,0 +1,14 @@
+
+
+def wrap(func):
+    def inner(a,b):
+        if b == 0:
+            a,b = b,a
+        return func(a,b)
+    return inner
+
+@wrap
+def div(a,b):
+    return a/b
+
+print(div(18,6))
